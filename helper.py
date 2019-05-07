2db34aacd2724b0b0cfb114356710f16775dd39b
Pere Lluis Huguet Cabot <phuguetcabot@amsterdamdatacollective.com>
2019-05-08 01:01:00 +0200

    added tensorboard

--- helper.py
+++ helper.py
@@ -1,14 +1,17 @@
 import numpy as np
 import torch
-def evaluate(evaluation_dataloader, model, criterion):
-    
+
+def evaluate(evaluation_dataloader, model, criterion, device):
     model.eval()
     # total_examples = total number of words
     total_examples = 0
     total_eval_loss = 0
     confusion_matrix = np.zeros((2, 2))
     for (eval_text, eval_lengths, eval_labels) in evaluation_dataloader:
-   
+        if torch.cuda.is_available():
+            eval_text.to(device=device)
+            eval_lengths.to(device=device)
+            eval_labels.to(device=device)
         # predicted shape: (batch_size, seq_len, 2)
         predicted = model(eval_text, eval_lengths)
         # Calculate loss for this test batch. This is averaged, so multiply
@@ -21,18 +24,25 @@ def evaluate(evaluation_dataloader, model, criterion):
         confusion_matrix = update_confusion_matrix(confusion_matrix, predicted_labels, eval_labels.data, eval_lengths)
     average_eval_loss = total_eval_loss / evaluation_dataloader.__len__()
     #print(evaluation_dataloader.__len__())
-    print(confusion_matrix)
     precision = 100 * confusion_matrix[1, 1] / np.sum(confusion_matrix[1])
-    print(precision)
     recall = 100 * confusion_matrix[1, 1] / np.sum(confusion_matrix[:, 1])
-    print(recall)
     f1 = 2 * precision * recall / (precision + recall)
     accuracy = 100 * (confusion_matrix[1, 1] + confusion_matrix[0, 0]) / np.sum(confusion_matrix)
     # Set the model back to train mode, which activates dropout again.
     model.train()
     return average_eval_loss.data.item(), precision, recall, f1, accuracy
+
+def evaluate_train(labels, predictions, lengths):
+    _, predicted_labels = torch.max(predictions.data, 2)
+    confusion_matrix = np.zeros((2, 2))
+    confusion_matrix = update_confusion_matrix(confusion_matrix, predicted_labels, labels.data, lengths)
+    precision = 100 * confusion_matrix[1, 1] / np.sum(confusion_matrix[1])
+    recall = 100 * confusion_matrix[1, 1] / np.sum(confusion_matrix[:, 1])
+    f1 = 2 * precision * recall / (precision + recall)
+    accuracy = 100 * (confusion_matrix[1, 1] + confusion_matrix[0, 0]) / np.sum(confusion_matrix)
+    return precision, recall, f1, accuracy
+
 def update_confusion_matrix(matrix, predictions, labels, sen_len):
-    
     for i in range(len(sen_len)):
         prediction = predictions[i]
         label = labels[i]
--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from datasets import SentenceDataset,DocumentDataset
 from Data.Metaphors.embeddings import extract_emb
 import torch.utils.data as data_utils
 from model import BiLSTMEncoder,MainModel
-from helper import evaluate
+from helper import evaluate, evaluate_train
 import torch.nn as nn
 import torch.optim as optim
+import torch
+from tensorboardX import SummaryWriter
 
 def load_elmo_dataset(path, max_len=200):
     '''
@@ -41,28 +43,33 @@ def load_elmo_dataset(path, max_len=200):
     return np.array(X), np.array(Y)
 
 if __name__ == "__main__":
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
     batch_size = 64
-    hidden_dim = 512
-    emb_file = 'Data/Metaphors/meta_embeds.npy'
-    lab_file = 'Data/Metaphors/meta_labels.npy'
+    hidden_dim = 300
+    emb_file = 'Data/Metaphors/VUA/vua_train_embeds.npy'
+    lab_file = 'Data/Metaphors/VUA/vua_train_labels.npy'
+    emb_file_val = 'Data/Metaphors/VUA/vua_val_embeds.npy'
+    lab_file_val = 'Data/Metaphors/VUA/vua_val_labels.npy'
+
     data, labels = extract_emb(emb_file, lab_file)
-    #data, labels = load_elmo_dataset(path,200)
+    data_val, labels_val = extract_emb(emb_file_val, lab_file_val)
+
     dataset = SentenceDataset(data, labels, 200)
-    loader_doc = DocumentDataset(data, labels, 200)
-    train_size = int(0.8 * len(dataset))
-    test_size = len(dataset) - train_size
-    train_dataset, test_dataset = data_utils.random_split(dataset, [train_size, test_size])
+    dataset_val = SentenceDataset(data_val, labels_val, 200)
 
-    loader_dataset = data_utils.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+    loader_dataset = data_utils.DataLoader(dataset, batch_size=batch_size, shuffle=True,
                                   collate_fn=SentenceDataset.collate_fn)
-    val_dataset = data_utils.DataLoader(test_dataset, batch_size=batch_size, shuffle=True,
+    val_loader = data_utils.DataLoader(dataset_val, batch_size=batch_size, shuffle=True,
                                   collate_fn=SentenceDataset.collate_fn)
 
-    model = MainModel(embed_dim=1024, hidden_dim = hidden_dim, layers = 1, dropout_lstm = 0, dropout_input=0, dropout_FC=0, num_classes = 2)
-
+    model = MainModel(embed_dim=1024, hidden_dim = hidden_dim, layers = 1, dropout_lstm = 0, dropout_input=0.5, dropout_FC=0.1, num_classes = 2)
+    if torch.cuda.is_available():
+        model.to(device=device)
     nll_criterion = nn.NLLLoss()
     # Set up an optimizer for updating the parameters of the rnn_clf
-    met_model_optimizer = optim.SGD(model.parameters(), lr=0.01,momentum=0.9)
+    met_model_optimizer = optim.Adam(model.parameters(), lr=0.005)
     # Number of epochs (passes through the dataset) to train the model for.
     num_epochs = 20
 
@@ -70,20 +77,37 @@ if __name__ == "__main__":
     val_f1 = []
     counter = 0
 
+    writer = SummaryWriter()
+
     for epoch in range(num_epochs):
         print("Starting epoch {}".format(epoch + 1))
         for (data, lengths, labels) in loader_dataset:
+            if torch.cuda.is_available():
+                data.to(device=device)
+                lengths.to(device=device)
+                labels.to(device=device)
             predicted = model(data, lengths)
             batch_loss = nll_criterion(predicted.view(-1, 2), labels.view(-1))
-            print(batch_loss.item())
+            precision, recall, f1, eval_accuracy = evaluate_train(labels, predicted, lengths)
             met_model_optimizer.zero_grad()
             batch_loss.backward()
             met_model_optimizer.step()
             counter += 1
+            writer.add_scalar('Train/F1', f1, (counter))
+            writer.add_scalar('Train/precision', precision, (counter))
+            writer.add_scalar('Train/recall', recall, (counter))
+            writer.add_scalar('Train/accuracy', recall, (counter))
+            writer.add_scalar('Train/Loss', batch_loss.item(), (counter))
             if counter % 2 == 0:
-                avg_eval_loss, precision, recall, f1, eval_accuracy = evaluate(val_dataset, model, nll_criterion)
+                avg_eval_loss, precision, recall, f1, eval_accuracy = evaluate(val_loader, model, nll_criterion, device)
+                writer.add_scalar('Val/F1', f1, (counter))
+                writer.add_scalar('Val/precision', precision, (counter))
+                writer.add_scalar('Val/recall', recall, (counter))
+                writer.add_scalar('Val/accuracy', eval_accuracy, (counter))
+                writer.add_scalar('Val/Loss', avg_eval_loss, (counter))
                 #print(avg_eval_loss)
                 #val_loss.append(avg_eval_loss)
                 #val_f1.append(f1)
                 print("Iteration {}. Validation Loss {}. Validation Accuracy {}. Validation Precision {}. Validation Recall {}. Validation F1 {}.".format(counter, avg_eval_loss, eval_accuracy, precision, recall, f1))
     print("Training done!")
+    writer.close()
